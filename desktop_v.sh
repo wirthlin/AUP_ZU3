5949b9a9919bb12aaaddda618c5e17f4cf7adac9
Mike Wirthlin <wirthlin@byu.edu>
2026-03-10 15:30:22 -0600

    Added script and moved document

---
+++ desktop_v.sh
@@ -0,0 +1,5 @@
+#source /tools/Xilinx/Vivado/2021.2/settings64.sh
+source /mnt/toolsdisk/Xilinx/Vivado/2024.1/settings64.sh
+# Modelsim
+#export PATH=$PATH:/mnt/toolsdisk/questasim/bin
+#export LM_LICENSE_FILE=1717@ece-modelsim.byu.edu
